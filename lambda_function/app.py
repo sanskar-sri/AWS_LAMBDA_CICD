4e187322ba1c5e6d669a86c12edf457bd4ee26f8
sanskar srivastava <sanskaranmol786@gmail.com>
2026-08-20 00:34:00 +0530

    added coded in app.py

--- lambda_function/app.py
+++ lambda_function/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 def lambda_handler(event,context):
+    print("deploy lamda code from github")
     response=requests.get('https://jsonplaceholder.typicode.com/posts/1')
     data=response.json()
     
