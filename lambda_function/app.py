cf6a0e28ba0f9747ab47bec8f2b69c08d1b4addd
sanskar srivastava <sanskaranmol786@gmail.com>
2026-08-19 21:49:50 +0530

    modeified lambda_function

---
+++ lambda_function/app.py
@@ -0,0 +1,28 @@
+
+import os
+import requests
+import pandas as pd  
+
+
+def lambda_handler(event,context):
+    response=requests.get('https://jsonplaceholder.typicode.com/posts/1')
+    data=response.json()
+    
+    df=pd.DataFrame(data)
+    print(df.head())
+    
+    
+    ##We also required env var to register in lambda function
+    print("Environment Variable:")
+    defined_variables=["ENV","API_KEY","LOG_LEVEL"]
+    
+    for key in defined_variables:
+        value=os.environ.get(key)
+        if value is not None:
+            print(f"{key}:{value}")
+            
+    return  {
+                "StatusCode":200,
+                "body": "Lambda function executed successfully"
+        }
+    
